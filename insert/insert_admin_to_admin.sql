7b56bbede82b253aa04d7ea0873b937306a42d25
Phạm Quang Tiến Thành <164118104+Thanhizme@users.noreply.github.com>
2025-11-16 10:38:36 +0700

    Add files via upload

---
+++ insert/insert_admin_to_admin.sql
@@ -0,0 +1,10 @@
+USE [database_systems_asm2];
+GO
+
+INSERT INTO [Admin] (University_ID, [Type])
+VALUES
+(9001, N'Coordinator'),
+(9002, N'Office of Academic Affairs'),
+(9003, N'Office of Student Affairs'),
+(9004, N'Program Administrator');
+GO
---
+++ insert/insert_admin_to_user.sql
@@ -0,0 +1,15 @@
+﻿USE [database_systems_asm2];
+GO
+
+
+INSERT INTO [Users] (University_ID, First_Name, Last_Name, Email, National_ID)
+VALUES
+
+(9001, N'An', N'Nguyễn Văn', N'admin.an@hcmut.edu.vn', N'000000009001'),
+
+(9002, N'Bình', N'Trần Văn', N'admin.binh@hcmut.edu.vn', N'000000009002'),
+
+(9003, N'Cường', N'Lê Văn', N'admin.cuong@hcmut.edu.vn', N'000000009003'),
+
+(9004, N'Dung', N'Phạm Thị', N'admin.dung@hcmut.edu.vn', N'000000009004');
+GO
